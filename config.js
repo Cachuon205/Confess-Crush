b8a20a711a1e37871825f16b44ceaa73b26e17ad
Cachuon205 <101097505+Cachuon205@users.noreply.github.com>
2022-03-07 18:44:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Nacker bot",
-    introTitle: 'Nguyễn Thu Hiền',
+    titleWeb: "Cá Chuồn",
+    introTitle: 'Nguyễn Tú Phương',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: '^^YêuAnh^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnYes: 'Yêu anh😘 <33',
+    btnNo: 'Không Yêu 🥺 :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Anh biết mà 🥰. Yêu em rất nhiều 😘😘',
+    messDesc: 'Tối nay 7h, call nch với anh nhaa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://facebook.com/chuon.ca.90' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
